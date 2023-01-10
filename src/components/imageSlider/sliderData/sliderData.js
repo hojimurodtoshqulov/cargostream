95fddd160b522e707c2b8dcbb2c432b559c22644
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-01-10 17:09:46 +0500

    showcase changed

--- src/components/imageSlider/sliderData/sliderData.js
+++ src/components/imageSlider/sliderData/sliderData.js
@@ -14,7 +14,7 @@ export const SliderData = [
 	},
 	{
 		// image:"https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80",
-		image: carouselImgTwo,
+		image: carouselImg4,
 		title: "International cargo shipment",
 		description: "Tranportation of goods anywhere in the world",
 		buttonTitle: "оставить заяавку",
@@ -28,8 +28,7 @@ export const SliderData = [
 		key: 2,
 	},
 	{
-		image:
-			carouselImg4,
+		image:carouselImgTwo,
 		title: "International cargo shipment",
 		description: "Tranportation of goods anywhere in the world",
 		buttonTitle: "оставить заяавку",
