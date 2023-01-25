5cfd172f1405bfb9aba8cc02c7528df909c94692
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-01-26 03:28:58 +0500

    news page was changed

--- src/pages/news/news.js
+++ src/pages/news/news.js
@@ -9,14 +9,70 @@ import bgImg from "../../media/news.jpg";
 import Slider from "react-slick";
 import { useState, useEffect } from "react";
 import { useRef } from "react";
-import { newsData } from "./newsData/newsData";
+// import { newsData } from "./newsData/newsData";
 import i18next from "i18next";
 import { useTranslation } from "react-i18next";
+import serviceCardImg1 from "../../media/transportation-and-logistics-of-container-3f334ac6.jpeg";
+import serviceCardImg2 from "../../media/news.jpg";
+import serviceCardImg3 from "../../media/shipping-rates-are-finally-falling-1428282157-standard-1536x1536.jpg";
+import serviceCardImg4 from "../../media/news12.jpg";
+import serviceCardImg5 from "../../media/transportnaya-logistika.jpg";
+import serviceCardImg6 from "../../media/pat-whelen-IcHxE93NoB8-unsplash.jpg";
 const News = () => {
 	const { t } = useTranslation();
 	const handleClick = (e) => {
 		i18next.changeLanguage(e.target.value);
 	};
+	const newsData = [
+		{
+			img: serviceCardImg1,
+			title: t("newsTitle1"),
+			description:t("newsTitle1Description"),
+			aos: "fade-right",
+			key: "01",
+		},
+		{
+			img: serviceCardImg2,
+			title: t("newsTitle2"),
+			description:
+				t("newsTitle2Description"),
+			aos: "fade-left",
+			key: "02",
+		},
+		{
+			img: serviceCardImg3,
+			title: t("newsTitle3"),
+			description:
+				t("newsTitle3Description"),
+			aos: "fade-right",
+			key: "03",
+		},
+		{
+			img: serviceCardImg4,
+			title: t("newsTitle4"),
+			description:
+				t("newsTitle4Description"),
+			aos: "fade-left",
+			key: "04",
+		},
+		{
+			img: serviceCardImg5,
+			title: t("newsTitle5"),
+			description:
+				t("newsTitle5Description"),
+			aos: "fade-right",
+			key: "05",
+		},
+		{
+			img: serviceCardImg6,
+			title: t("newsTitle6"),
+			description:
+				t("newsTitle6Description"),
+			aos: "fade-left",
+			key: "06",
+		},
+	];
+
 	const windowSize = useRef([window.innerWidth, window.innerHeight]);
 	const [num, setNum] = useState(4);
 	const listenScrollEvent = () => {
@@ -49,7 +105,7 @@ const News = () => {
 	return (
 		<Suspense fallback="loading">
 			<div className="main news">
-				<Nav onClick={(e) => handleClick(e)}/>
+				<Nav onClick={(e) => handleClick(e)} />
 				<input type="checkbox" id="navLinks__menu" />
 				<NavbarLinks />
 				<label htmlFor="navLinks__menu" className="navLinks__menu"></label>
