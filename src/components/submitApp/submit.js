b01cd4b43ab83daa5df2b15c5441625ffa45fa71
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-02-01 12:54:04 +0500

    toasty have been added

--- src/components/submitApp/submit.js
+++ src/components/submitApp/submit.js
@@ -2,13 +2,25 @@ import React, { useState } from "react";
 import Button from "../button/button";
 import "./submit.scss";
 import { useTranslation } from "react-i18next";
-// import { Axios } from "axios";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Submit = ({ onClose }) => {
-	// open={openModal} onClose={() => setOpenModal(false)}
+	const notify = () =>
+		toast.success("Your comment has been sent successfully!", {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "colored",
+		});
 	const [openModal, setOpenModal] = useState(false);
+	const [openSuccess, setOpenSuccess] = useState(false);
 	const navigate = useNavigate();
 	const url = "https://cargo-stream.herokuapp.com/email/send";
 	const [data, setData] = useState({
@@ -44,52 +56,78 @@ const Submit = ({ onClose }) => {
 		});
 		setOpenModal(false);
 		onClose(openModal);
+		setOpenSuccess(true);
+		notify(true);
+		<ToastContainer />;
 	};
 	const { t } = useTranslation();
 	return (
-		<div className="submit">
-			<h2>{t("submitTitle")}</h2>
-			<p>{t("submitDescription")}</p>
-			<form action="" className="form" onSubmit={(e) => submit(e)}>
-				<input
-					className="input-tell form-control"
-					onChange={(e) => handle(e)}
-					id="phone"
-					value={data.phone}
-					type="text"
-					placeholder={t("submitPhone")}
-					required
-				/>
-				<input
-					className="input-price form-control"
-					onChange={(e) => handle(e)}
-					id="price"
-					value={data.price}
-					type="text"
-					placeholder={t("submitPrice")}
-					required
-				/>
-				<input
-					className="input-name form-control"
-					onChange={(e) => handle(e)}
-					id="product"
-					value={data.product}
-					type="text"
-					placeholder={t("submitProduct")}
-					required
-				/>
-				<textarea
-					onChange={(e) => handle(e)}
-					id="comment"
-					value={data.comment}
-					name=""
-					cols="50"
-					rows="1"
-					placeholder={t("submitComments")}
-				></textarea>
-				<Button buttonTitle={t("submitTitle")} />
-			</form>
-		</div>
+		<>
+			<div className="submit">
+				<h2>{t("submitTitle")}</h2>
+				<p>{t("submitDescription")}</p>
+				<form action="" className="form" onSubmit={(e) => submit(e)}>
+					<input
+						className="input-tell form-control"
+						onChange={(e) => handle(e)}
+						id="phone"
+						value={data.phone}
+						type="text"
+						placeholder={t("submitPhone")}
+						required
+					/>
+					{/* {console.log("data phone ----> ", data.phone)} */}
+					<input
+						className="input-price form-control"
+						onChange={(e) => handle(e)}
+						id="price"
+						value={data.price}
+						type="text"
+						placeholder={t("submitPrice")}
+						required
+					/>
+					<input
+						className="input-name form-control"
+						onChange={(e) => handle(e)}
+						id="product"
+						value={data.product}
+						type="text"
+						placeholder={t("submitProduct")}
+						required
+					/>
+					<textarea
+						onChange={(e) => handle(e)}
+						id="comment"
+						value={data.comment}
+						name=""
+						cols="50"
+						rows="1"
+						placeholder={t("submitComments")}
+					></textarea>
+					<span
+						onClick={
+							data.phone && data.price && data.product && data.comment !== ""
+								? notify
+								: null
+						}
+					>
+						<Button buttonTitle={t("submitTitle")} />
+					</span>
+				</form>
+			</div>
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="colored"
+			/>
+		</>
 	);
 };
 export default Submit;
